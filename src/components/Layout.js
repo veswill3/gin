51aed133d59fe4c01b90e92d8d8f169c79d4201b
Vesper Williams <veswill3@gmail.com>
2024-06-08 11:12:13 -0600

    add hack to keep bottom buttons visible even when keyboard opens

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,22 +1,51 @@
+/* eslint-disable no-mixed-operators */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
 
-const Layout = ({ children, leftActionProps, rightActionProps }) => (
-  <div className="layout">
-    <div className="layout-content">
-      {children}
-    </div>
-    <div className="row">
-      <span className="layout-action-left">
-        <Button fluid {...leftActionProps} />
-      </span>
-      <span className="layout-action-right">
-        <Button fluid {...rightActionProps} />
-      </span>
-    </div>
-  </div>
-);
+function updateHeight() {
+  const layoutViewport = document.getElementById('layoutViewport');
+  const bottomBar = document.getElementById('bottombar');
+  const viewport = window.visualViewport;
+  const offsetX = viewport.offsetLeft;
+  const offsetY =
+    viewport.height -
+    layoutViewport.getBoundingClientRect().height +
+    viewport.offsetTop;
+  bottomBar.style.transform = `translate(${offsetX}px,${offsetY}px) scale(${
+    1 / viewport.scale
+  })`;
+}
+
+class Layout extends React.Component {
+  componentDidMount() {
+    window.visualViewport.addEventListener('resize', updateHeight);
+    window.visualViewport.addEventListener('scroll', updateHeight);
+  }
+  componentWillUnmount() {
+    window.visualViewport.removeEventListener('resize', updateHeight);
+    window.visualViewport.removeEventListener('scroll', updateHeight);
+  }
+
+  render() {
+    const { children, leftActionProps, rightActionProps } = this.props;
+    return (
+      <div className="layout">
+        <div className="layout-content">{children}</div>
+        <div id="bottombar">
+          <div className="row">
+            <span className="layout-action-left">
+              <Button fluid {...leftActionProps} />
+            </span>
+            <span className="layout-action-right">
+              <Button fluid {...rightActionProps} />
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 Layout.propTypes = {
   leftActionProps: PropTypes.shape({
